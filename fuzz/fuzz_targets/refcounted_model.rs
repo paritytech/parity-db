afd3d09157bcaae186be495294df3b4e28440099
Tpt <thomas.tanon@parity.io>
2022-09-16 18:07:37 +0200

    Fuzzer: better code factorization

--- fuzz/fuzz_targets/refcounted_model.rs
+++ fuzz/fuzz_targets/refcounted_model.rs
@@ -8,11 +8,10 @@
 use arbitrary::Arbitrary;
 use libfuzzer_sys::fuzz_target;
 use parity_db_fuzz::*;
-use std::collections::{
-	btree_map::{BTreeMap, Entry},
-	HashMap,
+use std::{
+	collections::{btree_map::Entry, BTreeMap, HashMap},
+	path::Path,
 };
-use tempfile::tempdir;
 
 #[derive(Arbitrary, Debug, Clone, Copy)]
 enum Operation {
@@ -21,82 +20,64 @@ enum Operation {
 	Reference(u8),
 }
 
-#[derive(Arbitrary, Debug)]
-enum Action {
-	Transaction(Vec<Operation>),
-	Restart,
-}
+struct Simulator;
 
-fuzz_target!(|entry: (Config, Vec<Action>)| {
-	let (config, actions) = entry;
-	let dir = tempdir().unwrap();
-	let options = parity_db::Options {
-		path: dir.path().to_owned(),
-		columns: vec![parity_db::ColumnOptions {
-			ref_counted: true,
-			preimage: true,
-			compression: config.compression.into(),
-			btree_index: config.btree_index,
-			..parity_db::ColumnOptions::default()
-		}],
-		sync_wal: true,
-		sync_data: true,
-		stats: false,
-		salt: None,
-		compression_threshold: HashMap::new(),
-	};
-	let mut db = parity_db::Db::open_or_create(&options).unwrap();
-	let mut model = BTreeMap::<u8, u8>::default();
-	for action in actions {
-		// We apply the action on both the database and the model
-		match action {
-			Action::Transaction(operations) => {
-				db.commit_changes(operations.iter().copied().map(|op| {
-					(
-						0u8,
-						match op {
-							Operation::Set(k) => parity_db::Operation::Set(vec![k], vec![k]),
-							Operation::Dereference(k) => parity_db::Operation::Dereference(vec![k]),
-							Operation::Reference(k) => parity_db::Operation::Reference(vec![k]),
-						},
-					)
-				}))
-				.unwrap();
-				for op in operations {
-					match op {
-						Operation::Set(k) => {
-							*model.entry(k).or_default() += 1;
-						},
-						Operation::Dereference(k) => {
-							if let Entry::Occupied(mut e) = model.entry(k) {
-								let counter = e.get_mut();
-								*counter -= 1;
-								if *counter == 0 {
-									e.remove_entry();
-								}
-							}
-						},
-						Operation::Reference(k) =>
-							if let Entry::Occupied(mut e) = model.entry(k) {
-								*e.get_mut() += 1;
-							},
-					}
-				}
+impl DbSimulator for Simulator {
+	type Operation = Operation;
+	type Model = BTreeMap<u8, u8>;
+
+	fn build_options(config: &Config, path: &Path) -> parity_db::Options {
+		parity_db::Options {
+			path: path.to_owned(),
+			columns: vec![parity_db::ColumnOptions {
+				ref_counted: true,
+				preimage: true,
+				compression: config.compression.into(),
+				btree_index: config.btree_index,
+				..parity_db::ColumnOptions::default()
+			}],
+			sync_wal: true,
+			sync_data: true,
+			stats: false,
+			salt: None,
+			compression_threshold: HashMap::new(),
+		}
+	}
+
+	fn apply_operation_on_model(operation: &Operation, model: &mut Self::Model) {
+		match *operation {
+			Operation::Set(k) => {
+				*model.entry(k).or_default() += 1;
 			},
-			Action::Restart =>
-				db = {
-					drop(db);
-					parity_db::Db::open_or_create(&options).unwrap()
+			Operation::Dereference(k) =>
+				if let Entry::Occupied(mut e) = model.entry(k) {
+					let counter = e.get_mut();
+					*counter -= 1;
+					if *counter == 0 {
+						e.remove_entry();
+					}
+				},
+			Operation::Reference(k) =>
+				if let Entry::Occupied(mut e) = model.entry(k) {
+					*e.get_mut() += 1;
 				},
 		}
-
-		assert_db_and_model_are_equals(
-			&db,
-			model
-				.iter()
-				.filter_map(|(k, c)| if *c > 0 { Some((*k, *k)) } else { None })
-				.collect::<Vec<_>>(),
-			config.btree_index,
-		);
 	}
+
+	fn map_operation(operation: &Operation) -> parity_db::Operation<Vec<u8>, Vec<u8>> {
+		match *operation {
+			Operation::Set(k) => parity_db::Operation::Set(vec![k], vec![k]),
+			Operation::Dereference(k) => parity_db::Operation::Dereference(vec![k]),
+			Operation::Reference(k) => parity_db::Operation::Reference(vec![k]),
+		}
+	}
+
+	fn model_content(model: &BTreeMap<u8, u8>) -> Vec<(Vec<u8>, Vec<u8>)> {
+		model.iter().map(|(k, _)| (vec![*k], vec![*k])).collect::<Vec<_>>()
+	}
+}
+
+fuzz_target!(|entry: (Config, Vec<Action<Operation>>)| {
+	let (config, actions) = entry;
+	Simulator::simulate(config, actions).unwrap();
 });
