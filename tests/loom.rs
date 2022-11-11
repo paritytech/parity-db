8e0ace9fa86c5437e0151d4179ada5c444dba597
Tpt <thomas.tanon@parity.io>
2022-11-11 11:39:21 +0100

    Loom: Adds an iteration test

--- tests/loom.rs
+++ tests/loom.rs
@@ -57,3 +57,49 @@ fn exec_simple_commit_and_write_concurrency(is_btree: bool, is_ref_counted: bool
 		assert_eq!(global_db.get(0, &[0]).unwrap(), global_db.get(0, &[1]).unwrap())
 	})
 }
+
+#[test]
+fn btree_iteration_concurrency() {
+	loom::model(move || {
+		let tmp = tempdir().unwrap();
+		let mut options = Options::with_columns(tmp.path(), 1);
+		options.always_flush = true;
+		options.with_background_thread = false;
+		options.salt = Some([0; 32]);
+		options.columns[0].btree_index = true;
+
+		let global_db = Arc::new(Db::open_or_create(&options).unwrap());
+
+		let db = global_db.clone();
+		let t1 = thread::spawn(move || {
+			db.commit::<_, Vec<u8>>(vec![
+				(0, vec![1], Some(vec![1])),
+				(0, vec![10], Some(vec![10])),
+			])
+			.unwrap();
+			db.commit::<_, Vec<u8>>(vec![
+				(0, vec![0], Some(vec![0])),
+				(0, vec![5], Some(vec![5])),
+				(0, vec![15], Some(vec![15])),
+			])
+			.unwrap();
+		});
+
+		let db = global_db.clone();
+		let t2 = thread::spawn(move || {
+			db.process_commits().unwrap();
+			db.flush_logs().unwrap();
+			db.enact_logs().unwrap();
+			db.clean_logs().unwrap();
+		});
+
+		let mut iter = global_db.iter(0).unwrap();
+		iter.seek_to_first().unwrap();
+		while iter.next().unwrap().is_some() {}
+		iter.seek_to_last().unwrap();
+		while iter.prev().unwrap().is_some() {}
+
+		t1.join().unwrap();
+		t2.join().unwrap();
+	})
+}
